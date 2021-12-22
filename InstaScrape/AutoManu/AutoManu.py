5220537ae2ef09b6662dab2d3a572f1b5ecae208
ozgunyargi <ozgun.yargi@gmail.com>
2021-12-22 12:48:16 +0300

    Followername scrape function is implamented.

--- InstaScrape/AutoManu/AutoManu.py
+++ InstaScrape/AutoManu/AutoManu.py
@@ -13,7 +13,6 @@ import wget
 import sys
 
 sys.path.append(os.getcwd())
-
 from InstaScrape.Config.config import *
 
 def open_site(path, site, browser_="Firefox"): # Opens site
@@ -151,9 +150,27 @@ def scroll_and_save(driver, counter): # Returns post URL's while scrolling down 
 
     return posts
 
+def followers(driver, acc_name):
+
+    driver.get(f"{SITE_}/{acc_name}")
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a'))).click()
+
+    try:
+        element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div[6]/div/div/div[2]/ul/div/li[1]")))
+        usernames = []
+        for i in range(1, 11):
+            element = driver.find_element_by_xpath(f"/html/body/div[6]/div/div/div[2]/ul/div/li[{str(i)}]")
+            element_html = element.get_attribute("innerHTML")
+            usernames.append(element_html[element_html.index("href")+len("href")+1:].split(" ")[0].replace('"', "").replace("/", ""))
+        return usernames
+
+    finally:
+        driver.quit()
+
 if __name__ == "__main__":
 
-    DRIVER_PATH = r"{}\chromedriver.exe".format(DRIVER_PATH_)
+    DRIVER_PATH = r"{}\chromedriver.exe".format(DRIVER_PATH_CHROME)
     SITE_NAME = SITE_
 
     USERNAME = USERNAME_
@@ -161,10 +178,10 @@ if __name__ == "__main__":
 
     SEARCH_KEY =PAGE_
     KEYWORD = "image"
+    account_name = "instagram"
 
-    driver = open_site(DRIVER_PATH, SITE_NAME)
-    login(USERNAME,PASSWORD, driver)
-    not_now(driver)
-    make_search(driver, SEARCH_KEY)
-    scroll(driver, 4)
-    posts = get_posts(driver)
+    driver_ = open_site(DRIVER_PATH, SITE_NAME, browser_="Chrome")
+    login(USERNAME,PASSWORD, driver_)
+    not_now(driver_)
+    followers(driver_, account_name)
+    driver_.close()
